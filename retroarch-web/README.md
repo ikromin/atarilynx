f6c3cd2889409e39b6fedb4f8322250ffc061e4a
Igor Kromin <igor@igorkromin.net>
2019-02-08 12:54:11 +1100

    Adding RetroArch based online Atari Lynx emultor for ROM testing purposes

---
+++ retroarch-web/README.md
@@ -0,0 +1,16 @@
+# Retroarch Web Emulator for Atari Lynx
+
+This is an Atari Lynx online emulator based on [RetroArch](https://www.retroarch.com).
+This emulator is intended for testing of Lynx ROMs.
+
+# Running
+
+The emulator must be run from a web browser. You will also need a web server to host the files. Any web server software should work. Simply point your web server document root to the directory where this emulator is located.
+
+# Usage
+
+Drag and drop your ROM into the box that displays 'Atari Lynx Emulator Ready' or alternatively click the 'Choose File' button. The ROM will be launched automatically once selected.
+
+Screen scale can be adjusted using the 1x, 2x, 3x and 4x links. Default scale is 2x.
+
+Keyboard controls are displayed on the right hand side of the emulator screen.
---
+++ retroarch-web/js/atga_play.js
@@ -0,0 +1,198 @@
+var canvas = $('#canvas');
+var dndArea = $('.at_play_dnd_area');
+var canvasScale = 2;
+
+var Module = {
+  noInitialRun: true,
+  print: function(text) { },
+  printErr: function(text) { },
+  preRun: [],
+  postRun: [],
+  canvas: canvas[0],
+  totalDependencies: 0,
+  locateFile: function(path, prefix) {
+    if (path == 'handy_libretro.wasm') {
+      return '/retroarch/handy_libretro.wasm';
+    }
+    return path;
+  },
+  onRuntimeInitialized: function() {
+    dndArea.text('Atari Lynx Emulator Ready');
+  }
+};
+
+function setupFileSystem()
+{
+  dndArea.text('Loading File System...');
+
+  var fsOpts = {
+    fs: "MountableFileSystem",
+    options: {
+      '/home/web_user/retroarch/userdata/content': {
+        fs: 'InMemory'
+      },
+      '/home/web_user/retroarch/userdata': {
+        fs: 'XmlHttpRequest',
+        options: {
+          baseUrl: '/retroarch/',
+          index: {
+            "retroarch.cfg":null,
+            "system":{
+              "lynxboot.img":null
+            }
+          },
+          preferXHR: true
+        }
+      }
+    }
+  };
+
+  BrowserFS.configure(fsOpts, function(error) {
+    if (error) {
+      dndArea.text('ERROR: ' + error);
+      return;
+    }
+    
+    var BFS = new BrowserFS.EmscriptenFS();
+    FS.mount(BFS, {root: '/home'}, '/home');
+  });
+}
+
+function uploadData(data, name)
+{
+   var dataView = new Uint8Array(data);
+   var fileName = '/home/web_user/retroarch/userdata/content/' + name;
+
+   FS.writeFile(fileName, dataView, { encoding: 'binary' });
+
+   Module['callMain']([fileName]);
+   dndArea.text('Running');
+   
+   $('canvas')[0].exitPointerLock();
+   fixCanvasSize();
+   
+   $('#at_rom_select').hide();
+}
+
+function fixCanvasSize() {
+  var w = 160;
+  var h = 102;
+
+  w *= canvasScale;
+  h *= canvasScale;
+
+  if (canvas.width() != w) {
+    canvas.css({
+      'width': w + 'px',
+      'height': h + 'px'
+    });
+    canvas.attr(
+      {
+        'width': w,
+        'height': h
+      }
+    );
+  }
+  setTimeout(fixCanvasSize, 2000);
+}
+
+function uploadFileFunc(droppedFiles) {
+  var file = droppedFiles[0];
+  var fname = file.name.toLowerCase();
+  
+  // make sure that a Lynx ROM file was dropped
+  if (fname.endsWith('.lnx') || fname.endsWith('.o') || fname.endsWith('.lyx')) {
+    dndArea.text('Loading ROM...');
+
+    var filereader = new FileReader();
+    filereader.file_name = file.name;
+    filereader.readAsArrayBuffer(file);
+    
+    filereader.onload = function() {
+      uploadData(this.result, this.file_name);
+    };
+    filereader.onerror = function() {
+      dndArea.text('Load ROM failed!')
+    }
+  }
+  else {
+  	dndArea.text('Please select a LNX ROM file!');
+  }
+}
+
+$(document).ready(function() {
+  dndArea.on('drag dragstart dragend dragover dragenter dragleave drop', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (event.type == 'dragenter') {
+      $(event.currentTarget).addClass('at_play_dnd_area_hover');
+    }
+    else if (event.type == 'dragleave' || event.type == 'dragend' || event.type == 'drop') {
+      $(event.currentTarget).removeClass('at_play_dnd_area_hover');
+    }
+    
+    if (event.type == 'drop') {
+      var droppedFiles = event.originalEvent.dataTransfer.files;
+      uploadFileFunc(droppedFiles);
+    }
+  });
+
+  $('#at_file_select').change(function(event) {
+    uploadFileFunc(event.target.files);
+  });
+
+  $(window).focusout(function() {
+    if (Browser.mainLoop.currentlyRunningMainloop) {
+      Module.pauseMainLoop();
+      dndArea.text('Paused');
+    }
+  });
+  $(window).focusin(function() {
+    if (Browser.mainLoop.scheduler == null && Browser.mainLoop.currentlyRunningMainloop) {
+      Module.resumeMainLoop();
+      dndArea.text('Running');
+    }
+  });
+
+  var keys = {
+    9: "tab",
+    13: "enter",
+    16: "shift",
+    18: "alt",
+    27: "esc",
+    33: "rePag",
+    34: "avPag",
+    35: "end",
+    36: "home",
+    37: "left",
+    38: "up",
+    39: "right",
+    40: "down",
+    112: "F1",
+    113: "F2",
+    114: "F3",
+    115: "F4",
+    116: "F5",
+    117: "F6",
+    118: "F7",
+    119: "F8",
+    120: "F9",
+    121: "F10",
+    122: "F11",
+    123: "F12"
+  };
+  window.addEventListener('keydown', function (e) {
+    if (keys[e.which]) {
+      e.preventDefault();
+    }
+  });
+
+  $('.at_canv_resize').click(function(e) {
+    e.preventDefault();
+    canvasScale = parseFloat($(e.target).data('value'));
+    fixCanvasSize();
+  });
+   
+  setupFileSystem();
+});
